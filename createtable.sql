35120808fb54d83c0ac68282fb3e3c304c5308a6
Duygu Kaya <91972556+duygukya@users.noreply.github.com>
2025-03-20 11:04:55 +0300

    .

--- createtable.sql
+++ createtable.sql
@@ -1,29 +1,29 @@
 USE foy3
-CREATE TABLE birimler (
-    birim_id INT PRIMARY KEY,
-    birim_ad CHAR(25) NOT NULL
-);
-CREATE TABLE calisanlar (
-    calisan_id INT PRIMARY KEY,
-    ad CHAR(25) NOT NULL,
-    soyad CHAR(25) NOT NULL,
-    maas INT,
-    katilmaTarihi DATETIME,
-    calisan_birim_id INT,
-    FOREIGN KEY (calisan_birim_id) REFERENCES birimler(birim_id) ON DELETE SET NULL
-);
-CREATE TABLE unvan (
-    unvan_calisan_id INT,
-    unvan_calisan CHAR(25) NOT NULL,
-    unvan_tarih DATETIME,
-    PRIMARY KEY (unvan_calisan_id),
-    FOREIGN KEY (unvan_calisan_id) REFERENCES calisanlar(calisan_id) ON DELETE CASCADE
-);
+CREATE TABLE Birimler (
+birim_id INT  PRIMARY KEY,
+birim_ad CHAR(25) NOT NULL
+)
 
-CREATE TABLE ikramiye (
-    ikramiye_calisan_id INT,
-    ikramiye_ucret INT,
-    ikramiye_tarih DATETIME,
-    PRIMARY KEY (ikramiye_calisan_id, ikramiye_tarih),
-    FOREIGN KEY (ikramiye_calisan_id) REFERENCES calisanlar(calisan_id) ON DELETE CASCADE
-);
+CREATE TABLE Calisanlar(
+calisan_id INT PRIMARY KEY,
+ad CHAR(25) NOT NULL,
+soyad CHAR(25) NOT NULL,
+maas INT ,
+katilmaTarihi DATETIME,
+calisan_birim_id INT,
+FOREIGN KEY (calisan_birim_id) REFERENCES Birimler(birim_id) ON DELETE SET NULL
+)
+
+CREATE TABLE İkramiye (
+ikramiye_calisan_id INT ,
+ikramiye_ucret INT,
+ikramiye_tarih DATETIME,
+FOREIGN KEY (ikramiye_calisan_id) REFERENCES Calisanlar(calisan_id) ON DELETE CASCADE
+)
+
+CREATE TABLE Unvan(
+unvan_calisan_id INT,
+unvan_calisan CHAR(25) NOT NULL,
+unvan_tarih DATETIME,
+FOREIGN KEY (unvan_calisan_id) REFERENCES Calisanlar(calisan_id) ON DELETE CASCADE 
+)
---
+++ insert.sql
@@ -0,0 +1,32 @@
+USE foy3;
+INSERT INTO Birimler (birim_id, birim_ad) VALUES
+(1, 'Yazılım'),
+(2, 'Donanım'),
+(3, 'Güvenlik');
+
+INSERT INTO Calisanlar (calisan_id, ad, soyad, maas, katilmaTarihi, calisan_birim_id) VALUES
+(1, 'İsmail', 'İşeri', 100000, '2014-02-20 00:00:00.000', 1),
+(2, 'Hami', 'Satılmış', 80000, '2014-06-11 00:00:00.000', 1),
+(3, 'Durmuş', 'Şahin', 300000, '2014-02-20 00:00:00.000', 2),
+(4, 'Kağan', 'Yazar', 500000, '2014-02-20 00:00:00.000', 3),
+(5, 'Meryem', 'Soydaldı', 500000, '2014-06-11 00:00:00.000', 3),
+(6, 'Duygu', 'Akşehir', 200000, '2014-06-11 00:00:00.000', 2),
+(7, 'Kübra', 'Seyhan', 75000, '2014-01-20 00:00:00.000', 1),
+(8, 'Gülcan', 'Yıldız', 90000, '2014-04-11 00:00:00.000', 3);
+
+INSERT INTO İkramiye (ikramiye_calisan_id, ikramiye_ucret, ikramiye_tarih) VALUES
+(1, 5000, '2016-02-20 00:00:00.000'),
+(2, 3000, '2016-06-11 00:00:00.000'),
+(3, 4000, '2016-02-20 00:00:00.000'),
+(1, 4500, '2016-02-20 00:00:00.000'),
+(2, 3500, '2016-06-11 00:00:00.000')
+
+INSERT INTO unvan (unvan_calisan_id, unvan_calisan, unvan_tarih) VALUES
+(1, 'Yönetici', CAST('2016-02-20 00:00:00.000' AS DATETIME)),
+(2, 'Personel', CAST('2016-06-11 00:00:00.000' AS DATETIME)),
+(8, 'Personel', CAST('2016-06-11 00:00:00.000' AS DATETIME)),
+(5, 'Müdür', CAST('2016-06-11 00:00:00.000' AS DATETIME)),
+(4, 'Yönetici Yardımcısı', CAST('2016-06-11 00:00:00.000' AS DATETIME)),
+(7, 'Personel', CAST('2016-06-11 00:00:00.000' AS DATETIME)),
+(6, 'Takım Lideri', CAST('2016-06-11 00:00:00.000' AS DATETIME)),
+(3, 'Takım Lideri', CAST('2016-06-11 00:00:00.000' AS DATETIME));
